b24c6d5ea7bd4b05101685f523afcbb1d9639c39
Daniel Victor <danielvictor@dialhost.com.br>
2019-05-16 09:50:42 -0300

    #1 ajustes finais em Usuario

--- app/Http/Controllers/UsuarioAdminController.php
+++ app/Http/Controllers/UsuarioAdminController.php
@@ -2,24 +2,53 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\EnderecoController;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
+use App\UsuarioAdmin;
+use App\Usuario;
 
 class UsuarioAdminController extends Controller
 {
-    public function CadastrarGestor(Request $request){
-        $data = new Usuario();
-        
-        $data->fill([
-            'nome_usuario' => 'daniel',
-            'nome' => 'nome',
-            'sobrenome' => 'sobrenome',
-            'cpf' => 10924707607,
-            'data_nascimento' => '28/08/1992',
-            'email' => 'email',
-            'senha' => Hash::make('usuario'),
-            'qtdanimais'=> 2
-            ]
-        );
+    
+    private $enderecoController;
+    
+    public function __construct(EnderecoController $enderecoController){
+        $this->enderecoController = $enderecoController;
     }
 
+    public function CadastrarGestor(Request $request){
+        
+        $endereco = $this->enderecoController->CadastrarEndereco($request);
+        if($endereco){
+            $data = new Usuario();
+            $data->fill([
+                'nome_usuario' => $request->input('nome_usuario'),
+                'nome' => $request->input('nome'),
+                'sobrenome' => $request->input('sobrenome'),
+                'cpf' => $request->input('cpf'),
+                'data_nascimento' => $request->input('data_nascimento'),
+                'email' => $request->input('email'),
+                'senha' => Hash::make($request->input('password')),
+                'qtdanimais'=> $request->input('qtdanimais'),
+                'id_endereco'=> $endereco
+                ]
+            );
+            if($data->save()){
+                $dataAdmin = new UsuarioAdmin();
+                $dataAdmin->fill([
+                    'nome_usuario' => $request->input('nome_usuario'),
+                    'CRMV' => $request->input('crmv'),
+                    'matricula' => $request->input('matricula')
+                    ]
+                );
+                if($dataAdmin->save()){  
+                    
+                    return response()->json(['code'=> 200 , 'message'=>'Usuário Cadastrado com Sucesso']);
+                }
+            }
+        }   
+        return response()->json(['code'=> 400 , 'message'=>'Erro ao cadastrar Usuário']);
+    }
+    
 }
